214d0642c1ca476f9d30a39c95825a87a9f90dc3
guillaume <guillaume.chausse@live.fr>
2017-05-23 14:53:33 +0200

    first-commit

---
+++ challenges/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * cv functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package challenges
+ */
+
+ // function additional_custom_styles() {
+ //
+ //     /*Enqueue The Styles*/
+ //    wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+ //    wp_enqueue_style( 'bootstrap');
+ //
+ // }
+
+ ?>
